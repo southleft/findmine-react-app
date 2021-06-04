12b73ac482e3648899f8358510460ef943e869f6
Christopher Snizik <csnizik@southleft.com>
2021-06-04 08:27:41 -0500

    add mock data

---
+++ src/server/defaultState.js
@@ -0,0 +1,107 @@
+export const defaultState = {
+    cardigans: [
+        {
+            "title": "Brian Bailey Long Body Open Cardigan",
+            "sku": "485-037",
+            "price": "$150.00",
+            "strikethrough": null,
+            "payments": "or 9 payments of $16.66",
+            "image": "cardigan01.png"
+        },
+        {
+            "title": "Sympli Go To Cardigan Long",
+            "sku": "395-293",
+            "price": "$179.00",
+            "strikethrough": null,
+            "payments": "or 9 payments of $19.88",
+            "image": "cardigan02.png"
+        },
+        {
+            "title": "Jennie Liu 100% Cashmere Button Front",
+            "sku": "339-693",
+            "price": "$160.00",
+            "strikethrough": null,
+            "payments": "or 9 payments of $17.78",
+            "image": "cardigan03.png"
+        }
+    ],
+    blouses: [
+        {
+            "title": "Brian Bailey Flutter Sleeve Blouse",
+            "sku": "371-420",
+            "price": "$120.00",
+            "strikethrough": null,
+            "payments": "or 9 payments of $13.33",
+            "image": "blouse01.png"
+        },
+        {
+            "title": "Alex Evenings Lace Blouse",
+            "sku": "948-328",
+            "price": "$124.00",
+            "strikethrough": "$159.00",
+            "payments": "or 9 payments of $13.77",
+            "image": "blouse02.png"
+        },
+        {
+            "title": "Parker Kitts Flutter Sleeve Wrap Front",
+            "sku": "196-983",
+            "price": "$156.00",
+            "strikethrough": null,
+            "payments": "or 9 payments of $17.33",
+            "image": "blouse03.png"
+        }
+    ],
+    shoes: [
+        {
+            "title": "Sam Edelman Argo Sneaker",
+            "sku": "998-383",
+            "price": "$99.88",
+            "strikethrough": "$160.00",
+            "payments": "or 9 payments of $11.10",
+            "image": "shoe01.png"
+        },
+        {
+            "title": "Soludos Yin Yang Sneakers",
+            "sku": "837-320",
+            "price": "$129.00",
+            "strikethrough": "$137.00",
+            "payments": "or 9 payments of $14.33",
+            "image": "shoe02.png"
+        },
+        {
+            "title": "4 inch Suede Espadrille Platform Wedge",
+            "sku": "293-876",
+            "price": "$77.00",
+            "strikethrough": null,
+            "payments": "or 9 payments of $8.55",
+            "image": "shoe03.png"
+        }
+    ],
+    clutches: [
+        {
+            "title": "Radley London Mill Bay Medium Flapover",
+            "sku": "597-993",
+            "price": "$269.99",
+            "payments": "or 9 payments of $29.99",
+            "strikethrough": null,
+            "image": "clutch01.png"
+        },
+        {
+            "title": "Tory Burch Kira Clutch",
+            "sku": "190-999",
+            "price": "$208.00",
+            "payments": "or 9 payments of $21.11",
+            "strikethrough": null,
+            "image": "clutch02.png"
+        },
+        {
+            "title": "Giuseppe Zanotti",
+            "sku": "779-103",
+            "price": "$319.00",
+            "payments": "or 9 payments of $35.44",
+            "strikethrough": "$507.00",
+            "image": "clutch03.png"
+        }
+    ]
+
+}
