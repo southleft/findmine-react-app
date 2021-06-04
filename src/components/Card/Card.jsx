9b6d930f4b5b4a84d1ac5f1e7607a17c7d46212a
Christopher Snizik <csnizik@southleft.com>
2021-06-04 09:19:59 -0500

    spacing

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,16 +6,18 @@ const Card = (props) => {
   return (
     <div className="card-content">
       <img src={`/assets/images/${props.image}`} alt={props.title} />
-      <p className="card-title">{props.title}</p>
-      <p className="card-sku">{props.sku}</p>
-      <p className="card-price">
-        {props.price}
-        {props.strikethrough !== null ? (
-          <span className="strikethrough">{props.strikethrough}</span>
-        ) : (
-          ''
-        )}
-      </p>
+      <div className="card-text">
+        <p className="card-title">{props.title}</p>
+        <p className="card-sku">{props.sku}</p>
+        <p className="card-price">
+          {props.price}
+          {props.strikethrough !== null ? (
+            <span className="strikethrough">{props.strikethrough}</span>
+          ) : (
+            ''
+          )}
+        </p>
+      </div>
     </div>
   );
 };
