f359c2598f469aa671ae661604334a772327ec84
Christopher Snizik <csnizik@southleft.com>
2021-06-06 15:31:53 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ import { store } from './store';
 import Desktop from './pages/Desktop';
 import Mobile from './pages/Mobile';
 import './assets/styles/index.css';
-
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 const App = () => {
   return (
     <Router>
       <Provider store={store}>
+        <Navbar bg="primary" variant="dark" fixed="top">
+          <Nav className="mr-auto">
+            <Navbar.Brand href="#"><h5>FindMine Demo</h5></Navbar.Brand>
+            <NavDropdown title="Change Viewport Size" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/">
+                Desktop (1204px wide)
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/mobile">
+                Mobile (436px wide)
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
         <Route exact path="/" component={Desktop} />
         <Route exact path="/mobile" component={Mobile} />
       </Provider>
